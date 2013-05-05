6f29f703a99d2c020f62d1fbfc56936eea4d96d3
Pascal Ellenberger <pascal.ellenberger@openmailbox.org>
2013-05-05 02:54:03 +0200

    Update zu Go, kleinere Verbesserungen der Leserlichkeit, unn?tige Schnipsel entfernt.

--- src/Go.java
+++ src/Go.java
@@ -1,37 +1,51 @@
-
-
+import java.util.StringTokenizer;
 public class Go extends Commands {
 
 	String name = "go";
-    //static int position;
-    String direction;
-    Checkinput check = new Checkinput();
 
 	public String getName(){
 		return name;
 	}
 	
-	String input = Checkinput.getInput();
-	
-	
-	
 
-	public static int getPosition(String input2){
-        String direction = Checkinput.input2;
-        System.out.println(Checkinput.input2);
-        System.out.println(direction);
-        int position = Player.position;
-        switch(direction.toLowerCase()){
-            case "north":position-=3; Player.setposition(position);
-            case "west":position-=1; Player.setposition(position);
-            case "south":position+=3; Player.setposition(position);
-            case "east":position+=1; Player.setposition(position);
-            default: return position;
-            
-           
-        }
+    public void checkdirection(String input){
+        Player player = new Player();
+    	StringTokenizer st = new StringTokenizer(input,"go ");
+		String input2 = st.nextToken();
+        int position = player.getPosition();
+
+//        switch(input2.toLowerCase()){
+//            case "north":
+//                position-=3;
+//            case "west":
+//                position-=1;
+//            case "south":
+//                position+=3;
+//            case "east":
+//                position+=1;
+//            default: System.out.println("Go where?");
+//        }
+        
+        
+        //GLeich wie kommentiert, nur mit elif und .equals()
+        if(input2.equalsIgnoreCase("north") == true){				//funktioniert nicht
+            position-=3;
+            }
+        else if(input2.equalsIgnoreCase("west") == true){			//funktioniert 
+            position-=1;
+            }
+        else if(input2.equalsIgnoreCase("south") == true){ 			//funktioniert
+        	position+=3;
+        	}
+        else if(input2.equalsIgnoreCase("east") == true){ 			//funktioniert nicht
+            position+=1;
+            }
+        else {
+        	System.out.println("Go where?");
+        	}
+        
+        player.setPosition(position);
+        System.out.println(position);
+
     }
-    
-
-	
 }
--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,22 @@
 import java.util.Scanner;
 
-
-
 public class Main {
 
 	public static void main(String args[]){	
-		Text text = new Text();
-		Go go = new Go();
-		Checkinput check = new Checkinput();
-		Scanner scan = new Scanner(System.in);
+        Text text = new Text();
+        Player player = new Player();
+        Scanner scan = new Scanner(System.in);
+        Checkinput check = new Checkinput();
 		System.out.println(text.getIntro());
 		while(true){
-		String input = scan.nextLine();
-		Checkinput.setInput(input);
-		System.out.println(check.Check(input));
-		//System.out.println(Go.getPosition(input));
-		}	
+            String input = scan.nextLine();
+            check.setInput(input);
+            check.Check(input);
+            if(player.dead == true){
+            	scan.close();
+            }
+		}
 		
-		//TODO: Generiert Liste mit allen Commands um diese 
-		//dann mit dem Spielerinput zu vergleichen.
-		//public Field[] getFields(Commands){
-		//}
 	}
 	
 	
