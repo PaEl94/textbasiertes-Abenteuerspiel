20717bcca614a3bbb06cebe9f10c3e2084089eff
Natriceus <Natriceus@localhost>
2013-09-01 15:13:55 +0200

    Drop f?gt Items jetzt dem entsprechenden room.content hinzu. Entfernung von veralteten Variabeln.

--- src/Drop.java
+++ src/Drop.java
@@ -1,45 +1,37 @@
 import java.util.ArrayList;
 import java.util.StringTokenizer;
 
-
-
-
 public class Drop extends Commands{
-	String name = "drop";
 	String input;
 	Inventory inventory;
 	boolean contains;
 	
 	
-	
-	
-	public Drop(String input,Inventory inv){
+	public Drop(String input,Inventory inv, ArrayList rooms, Rooms roomclass){
 		this.input = input;
 		this.inventory = inv;
 	}
-	
-	public String getName(){
-		return name;
-	}
-	
-	public String checkinventory(){
+		
+	public String checkInventory(ArrayList rooms, Rooms roomsclass){
 		StringTokenizer st = new StringTokenizer(input," ");
     	String input2 = st.nextToken();
 		input2 = st.nextToken();
-		
-		for(int i=0;i<inventory.inventory.size();i++){
-			if(inventory.inventory.get(i).getClass().getName().equalsIgnoreCase(input2)){
-				contains = true;
-				inventory.inventory.remove(i);
+		for (int x = 1; x < rooms.size()+1; x++){
+			if(x == Player.position){
+				for(int i=0;i<inventory.inventory.size();i++){
+					if(inventory.inventory.get(i).getClass().getName().equalsIgnoreCase(input2)){
+						contains = true;
+						roomsclass.getRoomContent(x-1).add(roomsclass.getRoomContent(x-1).get(i));
+						inventory.inventory.remove(i);
+						}
+					}
+				}
 			}
-		}
 		if(contains==false){
 			return "You have no item called " + input2 + " in your Inventory.";
 		}
 		else{
-			return "You dropped " +input2;
-		}
-		
+			return "You dropped " + input2;
+		}	
     }
-
 }
